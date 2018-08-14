27426bf82728b674f94922bfe948eb57a944ad00
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2018-08-14 18:05:53 +0100

    Script mejorado

--- 02_ejemplos/comprobar.sh
+++ 02_ejemplos/comprobar.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-find . -name "*.swift" -exec swiftc -typecheck -suppress-warnings {} \;
+FICHEROS=`find . -name "*.swift"`
+
+for F in $FICHEROS
+do
+	swiftc -typecheck -suppress-warnings "$F" > /dev/null 2>&1
+	ERROR=$?
+	if [ $ERROR -ne 0 ] 
+	then
+		echo "$F -> Error"
+	fi
+done
